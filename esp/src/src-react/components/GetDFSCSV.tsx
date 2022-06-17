93f8df1281907daed79bd618fba626aae342de9b
Gordon Smith <GordonJSmith@gmail.com>
2022-06-17 16:59:41 +0100

    WIP
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/src-react/components/GetDFSCSV.tsx
+++ esp/src/src-react/components/GetDFSCSV.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { DefaultButton, DetailsList, DetailsListLayoutMode, IColumn } from "@fluentui/react";
+import { SizeMe } from "react-sizeme";
+import { csvParse } from "d3-dsv";
 import { DaliService, WsDali } from "@hpcc-js/comms";
 import { scopedLogger } from "@hpcc-js/util";
 import { TableGroup } from "./forms/Groups";
 import nlsHPCC from "src/nlsHPCC";
-import { DefaultButton } from "@fluentui/react";
+import { HolyGrail } from "../layouts/HolyGrail";
 
-export class MyDaliService extends DaliService {
+class MyDaliService extends DaliService {
     GetDFSCSV(request: WsDali.GetDFSCSVRequest): Promise<WsDali.ResultResponse> {
         return this._connection.send("GetDFSCSV", request, "json", false, undefined, "ResultResponse");
     }
@@ -16,29 +19,51 @@ const logger = scopedLogger("src-react/components/GetDFSCSV.tsx");
 const myDaliService = new MyDaliService({ baseUrl: "" });
 
 interface GetDFSCSVProps {
-
 }
 
 export const GetDFSCSV: React.FunctionComponent<GetDFSCSVProps> = ({
 
 }) => {
 
-    const [dfsCsv, setDfsCsv] = React.useState<string>("");
+    const [columns, setColumns] = React.useState<IColumn[]>([]);
+    const [items, setItems] = React.useState<any[]>([]);
     const [logicalNameMask, setLogicalNameMask] = React.useState<string>("");
 
-    const onSubmit = React.useCallback(() => { myDaliService.GetDFSCSV({LogicalNameMask: logicalNameMask}).then(( ResultResponse ) => {
-        setDfsCsv(ResultResponse.Result);
-        console.log(ResultResponse);
-    }).catch(err => logger.error(err)); }, [logicalNameMask]);
+    const onSubmit = React.useCallback(() => {
+        myDaliService.GetDFSCSV({ LogicalNameMask: logicalNameMask }).then(response => {
+            const data = csvParse(response.Result);
+            setColumns(data.columns.map((col, idx) => {
+                return {
+                    key: col,
+                    name: col,
+                    fieldName: col,
+                    minWidth: 100
+                };
+            }));
+            setItems(data);
+        }).catch(err => logger.error(err));
+    }, [logicalNameMask]);
 
-    return <div>
-        <TableGroup fields={{
-        "LogicalNameMask": { label: nlsHPCC.LogicalNameMask, type: "string", value: logicalNameMask },
-            }} onChange={(id, value) => {
-            setLogicalNameMask (value);
-            }} />
-        <DefaultButton onClick= {onSubmit} text={nlsHPCC.Submit} />
-        {dfsCsv}
-    </div>;
-
+    return <HolyGrail
+        header={<span><TableGroup fields={{
+            "LogicalNameMask": { label: nlsHPCC.LogicalNameMask, type: "string", value: logicalNameMask },
+        }} onChange={(id, value) => {
+            setLogicalNameMask(value);
+        }} /><DefaultButton onClick={onSubmit} text={nlsHPCC.Submit} /></span>}
+        main={<SizeMe monitorHeight>{({ size }) => {
+            const height = `${size.height}px`;
+            return <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                <div style={{ position: "absolute", width: "100%", height: `${size.height}px` }}>
+                    <DetailsList compact={true}
+                        items={items}
+                        columns={columns}
+                        setKey="key"
+                        layoutMode={DetailsListLayoutMode.justified}
+                        selectionPreservedOnEmptyClick={true}
+                        styles={{ root: { height, minHeight: height, maxHeight: height } }}
+                    />
+                </div>
+            </div>;
+        }}</SizeMe>}
+    />;
 }; 
