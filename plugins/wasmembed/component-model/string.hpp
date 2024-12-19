c6f7764277a37c387fdedc246cb368fb36656a18
Gordon Smith <GordonJSmith@gmail.com>
2024-12-19 10:33:53 +0000

    WIP

--- plugins/wasmembed/component-model/string.hpp
+++ plugins/wasmembed/component-model/string.hpp
@@ -6,13 +6,13 @@
 
 namespace cmcpp
 {
-using offset = uint32_t;
-using size = uint32_t;
+    using offset = uint32_t;
+    using size = uint32_t;
 
-namespace string
-{
-    std::tuple<cmcpp::Encoding /*encoding*/, cmcpp::offset, cmcpp::size> load(const cmcpp::LiftLowerContext &cx, offset offset);
-}
+    namespace string
+    {
+        std::tuple<cmcpp::Encoding /*encoding*/, cmcpp::offset, cmcpp::size> load(const cmcpp::LiftLowerContext &cx, offset offset);
+    }
 }
 
 #endif
