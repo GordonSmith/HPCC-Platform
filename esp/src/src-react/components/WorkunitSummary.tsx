75d822de24c5d38856ab7dc2c5e58cebc2e63da6
Gordon Smith <GordonJSmith@gmail.com>
2026-05-23 19:01:53 +0100

    FluentUI v9: route WorkunitSummary mergeStyles import through @fluentui/style-utilities

--- esp/src/src-react/components/WorkunitSummary.tsx
+++ esp/src/src-react/components/WorkunitSummary.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { mergeStyles, registerIcons, ScrollablePane, ScrollbarVisibility } from "@fluentui/react";
+import { registerIcons, ScrollablePane, ScrollbarVisibility } from "@fluentui/react";
+import { mergeStyles } from "@fluentui/style-utilities";
 import { CommandBar, ContextualMenuItemType, ICommandBarItemProps } from "./CommandBarV9";
 import { Button, MessageBar, MessageBarActions, MessageBarBody, MessageBarIntent } from "@fluentui/react-components";
 import { DismissRegular } from "@fluentui/react-icons";
